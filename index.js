9b4456c49211cc4c79b59a7e21c6c8afc1e3ee1f
gopalbasak1 <gopalbasak1223@gmail.com>
2024-04-30 18:35:12 +0600

    fixed cors

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+  }
+  app.use(cors(corsConfig))
+
 app.use(express.json());
 
 
@@ -144,7 +150,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
